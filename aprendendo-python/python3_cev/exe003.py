58d4c0e9cf590659a44481c2ec9afa8be4091a87
ericjoji <ericjoji303@gmail.com>
2026-06-04 23:49:25 -0300

    feat(py3_mundo1_cev): criar exercício 3

---
+++ aprendendo-python/python3_cev/exe003.py
@@ -0,0 +1,11 @@
+# Exercício 3 - Aula 6
+# Utilize as aspas triplas (''' ''') ou hashtag (#) para comentar e testar cada comando linha a linha.
+
+# Fazer um programa que leia e some dois números inteiros e mostre o resultado da soma entre eles.
+
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+
+resultado_soma = num1 + num2
+
+print(f"A soma entre {num1} e {num2} é igual a {resultado_soma}")
