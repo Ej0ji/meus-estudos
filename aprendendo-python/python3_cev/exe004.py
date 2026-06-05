79d443cd22b6acf14e674ee141ce71316166c390
ericjoji <ericjoji303@gmail.com>
2026-06-05 00:20:13 -0300

    feat(py3_mundo1_cev): criar exercício 4

---
+++ aprendendo-python/python3_cev/exe004.py
@@ -0,0 +1,4 @@
+# Exercício 4 - Aula 6
+# Utilize as aspas triplas (''' ''') ou hashtag (#) para comentar e testar cada comando linha a linha.
+
+# O exercício 4 é o mesmo presente no desafio da aula 6, localizado em: (../python3_cev/dsfs_aula6.py)
