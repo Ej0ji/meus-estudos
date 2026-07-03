a97369dfe7bdcabed55bfab3528edb1aecd6c13b
ericjoji <ericjoji303@gmail.com>
2026-07-03 19:09:58 -0300

    feat(py3_mundo1_cev): criar exercício 5

---
+++ aprendendo-python/python3_cev/exe005.py
@@ -0,0 +1,11 @@
+# Exercício 5 - Aula 7
+# Utilize as aspas triplas (''' ''') ou hashtag (#) para comentar e testar cada comando linha a linha.
+
+# Ler um número inteiro e mostrar seu antecessor e seu sucessor
+
+numero_inteiro = int(input('Digite um número inteiro: '))
+print('_' * 30)
+print('O número digitado foi: {}.'.format(numero_inteiro))
+print('O seu antecessor inteiro é {}.'.format(numero_inteiro - 1))
+print('Já o seu sucessor, também inteiro, é {}.'.format(numero_inteiro + 1))
+
