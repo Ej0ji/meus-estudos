d275dcd6004baeccb504ba3962b919fbe8536582
ericjoji <ericjoji303@gmail.com>
2026-07-04 18:34:50 -0300

    feat(py3_mundo1_cev): criar exercício 12

---
+++ aprendendo-python/python3_cev/exe012.py
@@ -0,0 +1,9 @@
+# Exercício 12 - Aula 7
+# Utilize as aspas triplas (''' ''') ou hashtag (#) para comentar e testar cada comando linha a linha.
+
+# Ler o preço de um produto e mostrar seu novo preço com 5% de desconto.
+
+
+preco_produto = float(input('Digite o valor do produto: R$'))
+
+print('O produto de valor igual a R${}, com o desconto de 5%, passará a ter o valor de R${:.2f}.'.format(preco_produto, (preco_produto - preco_produto * 0.05)))
