4053608427a15e2df52153c4a530eda4452774fa
ericjoji <ericjoji303@gmail.com>
2026-07-04 20:07:05 -0300

    feat(py3_mundo1_cev): criar exercício 14 e 15

---
+++ aprendendo-python/python3_cev/exe014.py
@@ -0,0 +1,27 @@
+# Exercício 14 - Aula 7
+# Utilize as aspas triplas (''' ''') ou hashtag (#) para comentar e testar cada comando linha a linha.
+
+# Escrever um programa que converta uma temperatura digitada em °C e converta para °F.
+
+valor_graus_C = float(input('Digite o valor em graus Célsius: '))
+
+valor_C_para_F = (valor_graus_C * 1.8) + 32
+
+# A fórmula é assim devido à diferença no tamanho dos graus e no ponto de partida (zero) de cada escala.
+
+# A estrutura dividida explica essa relação matemática: A escala Celsius e a escala Fahrenheit possuem intervalos diferentes entre o ponto de congelamento e o ponto de ebulição da água:
+
+# Celsius: Vai de 0°C a 100°C (intervalo de 100 graus).
+# Fahrenheit: Vai de 32°F a 212°F (intervalo de 180 graus).
+ 
+# Dividindo o intervalo de Fahrenheit pelo de Celsius (180/100), chegamos à razão de 1,8. Isso significa que cada 1°C varia o equivalente a 1,8°F.
+
+# Agora, por que somar 32? (O ponto de partida)
+# As duas escalas não começam a contar o calor do mesmo ponto:
+
+# A água congela a 0°C.
+# A água congela a 32°F.
+
+# Como a escala Fahrenheit já começa no 32 quando a Celsius está no zero, você precisa somar 32 após ajustar o tamanho dos graus para corrigir esse "atraso".
+
+print('A temperatura atual em graus Célsius de {}°C, convertida para Fahrenheit, é de {:.1f}°F'.format(valor_graus_C, valor_C_para_F))
---
+++ aprendendo-python/python3_cev/exe015.py
@@ -0,0 +1,4 @@
+# Exercício 15 - Aula 7
+# Utilize as aspas triplas (''' ''') ou hashtag (#) para comentar e testar cada comando linha a linha.
+
+# Escrever um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado.
