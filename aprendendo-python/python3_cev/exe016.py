1d5f334c416aa75b810a635936198cd913e34cd0
ericjoji <ericjoji303@gmail.com>
2026-07-05 11:10:44 -0300

    style(py3_mundo1_cev): corrigir conjugação do enunciado

--- aprendendo-python/python3_cev/exe016.py
+++ aprendendo-python/python3_cev/exe016.py
@@ -1,7 +1,7 @@
 # Exercício 16 - Aula 8
 # Utilize as aspas triplas (''' ''') ou hashtag (#) para comentar e testar cada comando linha a linha.
 
-# Crie um programa que leia um número real qualquer inserido via teclado (ex: 6.127) e exiba na tela apenas a sua porção inteira (ex: 6). Dica: Explore as funções do módulo math.
+# Criar um programa que leia um número real qualquer inserido via teclado (ex: 6.127) e exiba na tela apenas a sua porção inteira (ex: 6). Dica: Explore as funções do módulo math.
 
 # IMPORTS
 
