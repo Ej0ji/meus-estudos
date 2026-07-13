ad287d8963e7b8ac6aa0c35a0bc22d95a9ce5ac4
ericjoji <ericjoji303@gmail.com>
2026-07-12 23:09:07 -0300

    feat(py3_mundo1_cev): criar segunda parte do exercício 19

--- aprendendo-python/python3_cev/exe019.py
+++ aprendendo-python/python3_cev/exe019.py
@@ -1,17 +1,29 @@
 # Exercício 19 - Aula 8
 # Utilize as aspas triplas (''' ''') ou hashtag (#) para comentar e testar cada comando linha a linha.
 
-# Escrever um programa que exiba aleatóriamente uma opção de nome dentre quatro nomes possíveis.
+# Escrever um programa que exiba aleatóriamente uma opção de nome dentre quatro nomes possíveis. Logo após exiba uma nova ordem dos quatro nomes.
 # DICA: Para este exercício será necessário o uso de listas
 
 # IMPORTAÇÕES
 
 from random import choice
+from random import shuffle
 
 # LÓGICA
 
 lista_nomes = ['Eric', 'Joji', 'Isshiki', 'Karai']
 
+print('Lista de nomes atual: {}.'.format(lista_nomes))
+
 nome_randomico = choice(lista_nomes)
 
-print(nome_randomico)
+print('Nome sorteado: {}.'.format(nome_randomico))
+
+# ------------------------------------------------------
+
+shuffle(lista_nomes)
+
+print('Nova ordem da lista de nomes: {}.'.format(lista_nomes))
+
+
+
