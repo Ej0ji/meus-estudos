d1dad1e4a8cd8bdaf680abc1b0739cfe11069bf3
ericjoji <ericjoji303@gmail.com>
2026-07-12 23:41:04 -0300

    feat(py3_mundo1_cev): criar exercício 20 e pasta de assets de exercícios

---
+++ aprendendo-python/python3_cev/exe020.py
@@ -0,0 +1,27 @@
+# Exercício 20 - Aula 8
+# Utilize as aspas triplas (''' ''') ou hashtag (#) para comentar e testar cada comando linha a linha.
+
+# Escrever um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+
+# RESOLUÇÃO 1 - Com pygame -------------------------------------------------
+
+# IMPORTAÇÕES
+
+import pygame
+
+# Inicializa o mixer do pygame
+pygame.mixer.init()
+
+# Carrega o arquivo MP3 (substitua pelo nome do seu arquivo)
+pygame.mixer.music.load('C:/Users/ericj/OneDrive/Documents/GitHub/meus-estudos/aprendendo-python/python3_cev/assets/faaah.mp3')
+
+# Inicia a reprodução
+pygame.mixer.music.play()
+
+# Mantém o programa rodando enquanto a música toca
+input('Pressione Enter para parar a música...')
+
+# RESOLUÇÃO 2 - Com playsound3 ---------------------------------------------
+
+# IMPORTAÇÕES
+
