75c337f5564146c4823775ca8c99a1842480b6d9
ericjoji <ericjoji303@gmail.com>
2026-07-18 14:15:01 -0300

    feat(py3_mundo1_cev): criar exercício 26

---
+++ aprendendo-python/python3_cev/exe026.py
@@ -0,0 +1,17 @@
+# Exercício 26 - Aula 9
+# Utilize as aspas triplas (''' ''') ou hashtag (#) para comentar e testar cada comando linha a linha.
+
+# Escrever um programa que leia o nome completo de uma pessoa e apresente, de forma separada, apenas o primeiro e o último nome.
+
+nome_completo = input('Digite um nome completo: ')
+
+nome_completo_transformado = nome_completo.title()
+
+indice_fim_primeiro_nome = nome_completo_transformado.find(' ')
+indice_comeco_ultimo_nome = nome_completo_transformado.rfind(' ') + 1
+
+print('-' * 45)
+
+print('''Olá, {}!
+Seu primeiro nome é {}.
+Já seu último nome é {}.'''.format(nome_completo_transformado, nome_completo_transformado[:indice_fim_primeiro_nome], nome_completo_transformado[indice_comeco_ultimo_nome:]))
