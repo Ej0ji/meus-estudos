dec20b21efce45637812438c8449fb56cdc42901
ericjoji <ericjoji303@gmail.com>
2026-07-18 00:48:01 -0300

    feat(py3_mundo1_cev): criar exercício 23

---
+++ aprendendo-python/python3_cev/exe023.py
@@ -0,0 +1,16 @@
+# Exercício 23 - Aula 9
+# Utilize as aspas triplas (''' ''') ou hashtag (#) para comentar e testar cada comando linha a linha.
+
+# Construir um script que leia o nome de uma cidade e informe se ela começa ou não com a palavra "Santo".
+
+nome_cidade = input('Digite o nome de uma cidade: ')
+
+nome_cidade_limpa = nome_cidade.strip()
+
+nome_cidade_transformada = nome_cidade_limpa.title()
+
+nome_comeca_com_santo = 'Santo' in nome_cidade_transformada[:(nome_cidade_transformada.find(' '))]
+
+print('-' * 30)
+
+print('A cidade com nome de {}, começa com a palavra "Santo"?: {}'.format(nome_cidade_transformada, nome_comeca_com_santo))
