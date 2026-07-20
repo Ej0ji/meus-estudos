828c0b2fd8c987c632406451bd81e9e8bf62b5c0
ericjoji <ericjoji303@gmail.com>
2026-07-20 00:00:01 -0300

    feat(py3_mundo1_cev): criar exercício 35

---
+++ aprendendo-python/python3_cev/exe035.py
@@ -0,0 +1,5 @@
+# Exercício 35 - Aula 11
+# Utilize as aspas triplas (''' ''') ou hashtag (#) para comentar e testar cada comando linha a linha.
+
+# -------------------------------- EXERCÍCIO FINAL ----------------------------------------
+# Estilizar as saídas de todos os exercícios anteriores utilizando os códigos de *sequential escape*
